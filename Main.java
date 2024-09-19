69bca92f6541c336ad14bba00c0f3990862f220a
Deependra-Ks01 <dksair02@icloud.com>
2024-09-19 23:25:52 +0530

    Basic Block chain created in java, added Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,65 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
-public class main {
-  public static void main(String[] args) {
-    String[] list1 = {"a", "b", "c"};
-    String[] list2 = {"a", "b", "c"};
+class Block {
+    private int previousHash;
+    private String[] transactions;
+    private int blockHash;
 
-    System.out.println(Arrays.hashCode(list1));
-    System.out.println(Arrays.hashCode(list2));
-  }
+    public Block(int previousHash, String[] transactions) {
+        this.previousHash = previousHash;
+        this.transactions = transactions;
+
+        Object[] contents = {Arrays.hashCode(transactions), previousHash};
+        this.blockHash = Arrays.hashCode(contents);
+    }
+
+    public int getBlockHash() {
+        return this.blockHash;
+    }
+
+    public int getPreviousHash() {
+        return this.previousHash;
+    }
+}
+
+class Blockchain {
+    private ArrayList<Block> chain = new ArrayList<>();
+
+    public Blockchain() {
+        String[] genesisTransactions = {"Genesis Block - Initial transactions"};
+        Block genesisBlock = new Block(0, genesisTransactions);  
+        chain.add(genesisBlock);
+    }
+
+    public void addBlock(String[] transactions) {
+        Block previousBlock = chain.get(chain.size() - 1);
+        int previousHash = previousBlock.getBlockHash();
+        Block newBlock = new Block(previousHash, transactions);
+        chain.add(newBlock);
+    }
+
+    public void printBlockchain() {
+        for (Block block : chain) {
+            System.out.println("Block:");
+            System.out.println("Previous Hash: " + block.getPreviousHash());
+            System.out.println("Block Hash: " + block.getBlockHash());
+            System.out.println();
+        }
+    }
+}
+
+public class Main { 
+    public static void main(String[] args) {
+        // Create the blockchain
+        Blockchain blockchain = new Blockchain();
+
+        String[] block1Transactions = {"Alice sent Bob 10 BTC", "Bob sent 5 BTC to Charlie"};
+        blockchain.addBlock(block1Transactions);
+
+        String[] block2Transactions = {"Charlie sent 2 BTC to Dave", "Dave sent 1 BTC to Eve"};
+        blockchain.addBlock(block2Transactions);
+
+        blockchain.printBlockchain();
+    }
 }
